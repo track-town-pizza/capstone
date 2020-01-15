0d8fab8323fe1bbd920f01fde036342450de6ef3
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-01-14 18:17:08 -0800

    Add Bootstrap, clean up source control

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import Button from 'react-bootstrap/Button';
+import React from "react"
 
 const Index = () => (
 	<div>
-		<Button>
-			<p>Hello, world!</p>
-		</Button>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+		<button type="button" className="btn btn-primary">Button</button>
 	</div>
-);
+)
 
-export default Index;
+export default Index
