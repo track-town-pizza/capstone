535d6897544fb6153cb47cd1f7297d47d1d2932e
hillkai <hillkai@oregonstate.edu>
2020-01-15 17:02:38 -0800

    Footer is working and is responsive to screen size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
+import Footer from "./footer"
 
 const Index = () => (
 	<div>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
-		<button type="button" className="btn btn-primary">Button</button>
+		<div>
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"></link>
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
+			<button type="button" className="btn btn-primary">Button</button>
+		</div>
+		<Footer />
 	</div>
 )
 
