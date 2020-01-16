2885a1e816ce109ac22694279cc7d2347a126579
AidenJN <nelsonai@oregonstate.edu>
2020-01-15 17:58:07 -0800

    Navbar collapse works decently well now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,33 +20,33 @@ const NavBar = props => (
 					<li className="nav-item">
 						<a href="#" className="link">Home</a>
 					</li>
-					
+
 					<li className="nav-item">
 						<a href="/menu" className="link">Menu</a>
 					</li>
-					
+
 					<li className="nav-item">
 						<a href="/contact" className="link">Contact</a>
 					</li>
-					
+
 					<li className="nav-item dropdown">
 						<a href="#" className="link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Community
 						</a>
-						
+
 						<div className="dropdown-menu" aria-labelledby="navbarDropdown">
 							<a href="/blog" className="dropdown-item">Blog</a>
 							<a href="/events" className="dropdown-item">Events</a>
 						</div>
 					</li>
-					
+
 					<li className="nav-item">
 						<a href="/about" className="link">About</a>
 					</li>
 				</ul>
 			</div>
 		</nav>
-		
+
 		<style jsx>{`
 			.link {
 				text-transform: uppercase;
@@ -67,7 +67,25 @@ const NavBar = props => (
 
 			.navbar-toggler-icon {
 				background-image: url(data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000…p='round' stroke-miterlimit='10' d='M47h22M4 15h22M423h22'/%3e%3c/svg%3e);
+                background-color: lime;
 			}
+
+            #navbar-content {
+                background-color: #007030;
+            }
+
+            // Potential sizing bug here with mobile styling
+            // style change is currently based off of browser's width
+            @media (max-width: 991px) {
+                #navbar-content {
+                    max-width: 25%;
+                    text-align: center;
+                    position: absolute;
+                    top: 60px;
+                    right: 0;
+                }
+            }
+
 		`}</style>
 	</div>
 )
