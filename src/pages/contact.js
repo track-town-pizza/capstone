a512244360ba9bba3af63d4101b396be0825663e
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-01-20 16:34:07 -0800

    Finish basic Contact page view

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,60 +9,47 @@ Props used in this page:
 */
 
 const Contact = props => {
-	const initMap = () => {
-		const trackTown = {
-			lat: 44.046211,
-			lng: -123.066509
-		}
-
-		const map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: trackTown
-		})
-	}
-
 	return (
-		<main>
+		<main className="w-50 mx-auto mt-3 d-flex flex-column">
 			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
+			<link href="https://fonts.googleapis.com/css?family=Oswald:200&display=swap" rel="stylesheet"></link>
 			<script async defer
 				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhrYAklW0znY-js2l2L5zuQWw23pPQDpo&callback=initMap"
 				type="text/javascript"
 			></script>
-			<div className="info-container">
-				<h1 className="title">Contact Us</h1>
+			<div className="d-flex justify-content-between">
+				<h1 className="title text-uppercase mb-4">Contact Us</h1>
 				{props.isAdminView && !props.isInEditMode &&
 					<button class="btn btn-primary">Edit</button>
 				}
 				{/* TODO: add conditional Save & Cancel buttons for being in admin edit mode */}
 			</div>
-			<div className="info-container">
-				<div className="location-info">
+			<div className="d-flex justify-content-between flex-wrap main-container">
+				<div className="text-left">
 					<h3>Location</h3>
 					{!props.isInEditMode &&
 						<p>{props.location}</p>
 					}
 				</div>
-				<div className="phone-info">
+				<div className="text-right">
 					<h3>Phone</h3>
 					{!props.isInEditMode &&
 						<p>{props.phone}</p>
 					}
 				</div>
+				<img src="tracktownpizza_map.png" 
+					alt="Track Town Pizza Google Map"
+					className="mw-100 mt-4 border border-secondary rounded" />
 			</div>
-			<div id="map"></div>
 			<style jsx>{`
 				.title {
 					color: #094c3a;
+					font-family: 'Oswald', sans-serif;
+					font-size: 60px;
 				}
 
-				.info-container {
-					display: flex;
-					justify-direction: space-between;
-				}
-
-				#map {
-					width: 300px;
-					height: 200px;
+				.content {
+					width: 40%;
 				}
 			`}</style>
 		</main>
