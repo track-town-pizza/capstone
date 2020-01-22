802dd00eb0a7fb7c732fdeede0a1ff8f86fe9f41
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-01-21 19:26:48 -0800

    Wrap Contact page with Layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React from "react"
+import Layout from "../components/Layout"
 
 /*
 Props used in this page:
@@ -8,23 +8,17 @@ Props used in this page:
 	- phone
 */
 
-const Contact = props => {
-	return (
-		<main className="w-50 mx-auto mt-3 d-flex flex-column">
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
-			<link href="https://fonts.googleapis.com/css?family=Oswald:200&display=swap" rel="stylesheet"></link>
-			<script async defer
-				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhrYAklW0znY-js2l2L5zuQWw23pPQDpo&callback=initMap"
-				type="text/javascript"
-			></script>
-			<div className="d-flex justify-content-between">
-				<h1 className="title text-uppercase mb-4">Contact Us</h1>
+const Contact = props => (
+	<Layout>
+		<div className="d-flex flex-column">					{/* Contents of Contact Page */}
+			<div className="d-flex justify-content-between">			{/* Title and Edit/Save/Cancel Buttons */}
+				<h1 className="title text-uppercase">Contact Us</h1>
 				{props.isAdminView && !props.isInEditMode &&
 					<button class="btn btn-primary">Edit</button>
 				}
 				{/* TODO: add conditional Save & Cancel buttons for being in admin edit mode */}
 			</div>
-			<div className="d-flex justify-content-between flex-wrap main-container">
+			<div className="d-flex justify-content-between flex-wrap main-container">		{/* Contact Info & Map */}
 				<div className="text-left">
 					<h3>Location</h3>
 					{!props.isInEditMode &&
@@ -39,23 +33,25 @@ const Contact = props => {
 				</div>
 				<img src="tracktownpizza_map.png" 
 					alt="Track Town Pizza Google Map"
-					className="mw-100 mt-4 border border-secondary rounded" />
+					className="mw-100 border border-secondary rounded" />
 			</div>
-			<style jsx>{`
-				.title {
-					color: #094c3a;
-					font-family: 'Oswald', sans-serif;
-					font-size: 60px;
-				}
+		</div>
+		
+		<style jsx>{`
+			.title {
+				color: #094c3a;
+				font-family: 'Oswald', sans-serif;
+				font-size: 60px;
+			}
 
-				.content {
-					width: 40%;
-				}
-			`}</style>
-		</main>
-	)
-}
+			.content {
+				width: 40%;
+			}
+		`}</style>
+	</Layout>
+)
 
+// These hardcoded values will eventually change to be programmatically determined
 Contact.getInitialProps = () => ({
 	isAdminView: false,
 	isInEditMode: false,
