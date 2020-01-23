b7d36d7338d459cd9e56cacbce307a348bf9308a
AidenJN <nelsonai@oregonstate.edu>
2020-01-22 16:57:52 -0800

    Added post sample data

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout"
+import PostData from "./PostData.json"
+
+const Index = () => (
+	<Layout>
+		<h1>Blog page coming soon...</h1>
+	</Layout>
+)
+
+export default Index
