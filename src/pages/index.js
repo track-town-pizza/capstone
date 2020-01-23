ed2eff82aba68db9a878f213103913e5c03a6365
hillkai <hillkai@oregonstate.edu>
2020-01-22 16:11:00 -0800

    added google maps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Footer from "./components/footer"
+import GoogleMaps from "./components/googleMaps"
 
 const Index = () => (
 	<div>
@@ -8,7 +9,8 @@ const Index = () => (
 			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
 			<button type="button" className="btn btn-primary">Button</button>
 		</div>
-		<Footer />
+		<GoogleMaps />
+		{/* <Footer /> */}
 	</div>
 )
 
