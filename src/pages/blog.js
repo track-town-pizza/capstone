5b338d76c056d751d0eaf6e63aedc7982803ae5f
AidenJN <nelsonai@oregonstate.edu>
2020-01-23 09:17:19 -0800

    Blog page renders static blog amount from json data

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout"
 import PostData from "./PostData.json"
+import SinglePost from "../components/SinglePost"
+
+const postRenderLimit = 3;
 
 const Index = () => (
 	<Layout>
-		<h1>Blog page coming soon...</h1>
+        <SinglePost post={PostData.posts[0]} />
+        <SinglePost post={PostData.posts[1]} />
+        <SinglePost post={PostData.posts[2]} />
 	</Layout>
 )
 
