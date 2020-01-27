b2c3ade6d6c72d7daef25698c9417cd57f40d2cd
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-01-27 11:25:00 -0800

    Render PostData on Blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,13 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
-import PostData from "../../PostData.json"
-import SinglePost from "../components/SinglePost"
+import postData from "../../PostData.json"
+// import SinglePost from "../components/SinglePost"
 
 const postRenderLimit = 3;
 
 const Blog = props => (
 	<Layout>
-		{/* {postData.Posts.map(post => (
+		{postData.posts.map(post => (
 			<div key={post.id}>
 				<h3>
 					<Link href="/post/[id]" as={`/post/${post.id}`}>
@@ -18,10 +18,10 @@ const Blog = props => (
 				<img src={post.imageLink} className="mw-100" />
 				<p>{post.content}</p>
 			</div>
-		))} */}
-        <SinglePost post={PostData.posts[0]} />
+		))}
+        {/* <SinglePost post={PostData.posts[0]} />
         <SinglePost post={PostData.posts[1]} />
-        <SinglePost post={PostData.posts[2]} />
+        <SinglePost post={PostData.posts[2]} /> */}
 	</Layout>
 )
 
