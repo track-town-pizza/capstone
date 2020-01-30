9374e0d6fd3e061af3c48dce0999d0c6647d333d
hillkai <hillkai@oregonstate.edu>
2020-01-30 00:19:42 -0800

    multiple menu items supported also mobile friendly

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -8,9 +8,73 @@ const Menu = () => {
 
     return (
         <Layout>
-            <h1 className="text-center mb-4">Track Town Menu</h1>
+            <h1 className="text-center">Track Town Menu</h1>
             <div className="menu-container">
-                {menuInfoCom}
+                <a href="#" className="menu-item">
+                    <img src="./photos/Pepperoni.jpg" alt="Track Town Pizza" />
+                    <div className="green-box">
+                        <p className="p-2 m-0 text-center">Pizza</p>
+                    </div>
+                </a>
+
+                <a href="#" className="menu-item">
+                    <img src="./photos/AllSticks.jpg" alt="Track Town Pizza" />
+                    <div className="green-box">
+                        <p className="p-2 m-0 text-center">Sides</p>
+                    </div>
+                </a>
+
+                <a href="#" className="menu-item">
+                    <img src="./photos/AllSauce.jpg" alt="Track Town Pizza" />
+                    <div className="green-box">
+                        <p className="p-2 m-0 text-center">Side Sauces</p>
+                    </div>
+                </a>
+
+                <a href="#" className="menu-item">
+                    <img src="./photos/AllSauce.jpg" alt="Track Town Pizza" />
+                    <div className="green-box">
+                        <p className="p-2 m-0 text-center">Sauce</p>
+                    </div>
+                </a>
+                
+                <style jsx>{`
+                    .menu-container {
+                        display: flex;
+                        flex-flow: row wrap;
+                        justify-content: space-around;
+                    }
+                    .menu-item {
+                        position: relative;
+                        width: 30%;
+                        margin-bottom:5%;
+                    }
+                    img {
+                        width: 100%;
+                        border-radius: 10px;
+                    }
+                    .green-box {
+                        position: absolute;
+                        bottom: 0px;
+                        background: rgb(0, 112, 48, 0.8);
+                        width: 40%;
+                        color: #FFFFFF;
+                        border-radius: 10px;
+                    }
+                    p {
+                        font-size:1.5vw;
+                    }
+
+                    @media only screen and (max-width: 600px) {
+                        .menu-item {
+                            width: 100%;
+                        }
+                        p {
+                            font-size:4vw;
+                        }
+                      }
+                `}</style>
+
             </div>
 
             <style jsx>{`
