e46dd4b346547690a9ab151876724e50b9898123
hillkai <hillkai@oregonstate.edu>
2020-01-29 20:57:32 -0800

    fixed up some styling issues in about and fixed it so it is reading from json

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import Layout from "../components/Layout"
 import Link from "next/link"
+import aboutData from "../../data/about.json"
 
 const About = () => {
     return (
         <Layout>
             <h1 className="text-center">About Track Town Pizza</h1>
             <div className="h-50 mx-auto">
-                <img src="./photos/a1.JPG" className="img-fluid" alt="Track Town Pizza" />
+                <img src={aboutData.imgLink} className="img-fluid rounded" alt="Track Town Pizza" />
             </div>
             <div className="mx-auto">
                 <p className="about-text">{aboutData.p1}</p>
@@ -14,10 +15,14 @@ const About = () => {
                 <p className="about-text">{aboutData.p3}</p>
                 <p className="about-text">{aboutData.p4}</p>   
             </div>
-            <div>
-                <h3 className="text-center">Checkout the rest of Track Town USA</h3>
-                <Link href="./TrackTownUSAMap.pdf">
-                    <a href="./TrackTownUSAMap.pdf" className="text-center">Track Town USA Map</a>
+            <div className="text-center">
+                <h3>Check out the rest of Track Town USA</h3>
+                <Link href={aboutData.pdfLink}>
+                    <a href={aboutData.pdfLink} className="text-success">Track Town USA Map</a>
+                </Link>
+                <br/>
+                <Link href="#">
+                    <a href="#" className="text-success">Track Town Pizza Blog</a>
                 </Link>
             </div>
         </Layout>
