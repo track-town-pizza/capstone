f3a6f7773fa99d456549d993498d9d3e7f2577e9
AidenJN <nelsonai@oregonstate.edu>
2020-01-29 17:01:40 -0800

    Separated single blog posts into their own components

---
+++ src/components/SinglePost.js
@@ -0,0 +1,37 @@
+import Link from "next/link"
+
+const SinglePost = (props) => {
+    const post = props.post
+    return (
+        <div className="SinglePost">
+            <div key={post.id}>
+                <h3>
+                    <Link href="/post/[id]" as={`/post/${post.id}`}>
+                        <a>{post.title}</a>
+                    </Link>
+                </h3>
+                <small>{post.date}</small>
+                <img src={post.imageLink} className="mw-100" />
+                <p>{post.content}</p>
+            </div>
+
+            <style jsx>{`
+                img {
+                    margin-top: 15px;
+                    margin-bottom: 15px;
+                }
+
+                .SinglePost {
+                    background: #F8F8F8;
+                    border: 1px solid #EDEDED;
+                    box-sizing: border-box;
+                    border-radius: 10px;
+                    padding: 20px;
+                    margin-bottom: 30px;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+export default SinglePost
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,15 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
 import postData from "../../PostData.json"
-// import SinglePost from "../components/SinglePost"
+import SinglePost from "../components/SinglePost"
 
 const postRenderLimit = 3;
 
 const Blog = props => (
 	<Layout>
 		{postData.posts.map(post => (
-			<div key={post.id}>
-				<h3>
-					<Link href="/post/[id]" as={`/post/${post.id}`}>
-						<a>{post.title}</a>
-					</Link>
-				</h3>
-				<small>{post.date}</small>
-				<img src={post.imageLink} className="mw-100" />
-				<p>{post.content}</p>
-			</div>
+            <SinglePost post={post} key={post.id}/>
 		))}
-        {/* <SinglePost post={PostData.posts[0]} />
-        <SinglePost post={PostData.posts[1]} />
-        <SinglePost post={PostData.posts[2]} /> */}
 	</Layout>
 )
 
