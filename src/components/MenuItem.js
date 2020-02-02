1b38eeef4fdd7953bbd3b576af5d0d2c4f172a5d
hillkai <hillkai@oregonstate.edu>
2020-02-01 22:07:10 -0800

    rough version of items is working with breaking it up into components

---
+++ src/components/MenuItem.js
@@ -0,0 +1,48 @@
+import React from "react"
+import ItemInfo from "./ItemInfo"
+
+const MenuItem = props => {
+    // console.log(typeof(props))
+    // const ItemInfoComponents = props.sidesInfo.map(sideInfo => (<ItemInfo sideInfo={sideInfo.sideInformation}/>))
+    return (
+        <a href="#" className="side-item">
+            <img src={props.sidesInfo.imgLink} alt="Track Town Pizza"/>
+            <ItemInfo itemInformation={props.sidesInfo.sideInformation}/>
+            <style jsx>{`
+                    .side-item {
+                        min-width: 200px;
+                        width: 25%;
+                        margin-bottom:5%;
+                        margin-right: 2.5%;
+                        margin-left: 2.5%;
+                    }
+                    img {
+                        width: 100%;
+                        border-radius: 10px 10px 0px 0px;
+                    }
+                    .description-box {
+                        background: #FFEC65;
+                        border-radius: 0px 0px 10px 10px;
+                        overflow: auto;
+                        color: #000000
+                    }
+                    .item-price-description {
+                        overflow: auto;
+                    }
+                    .item-description {
+                        float: left;
+                        margin-bottom: .5rem;
+                        margin-left: .5rem;
+                        margin-top: .5rem;
+                    }
+                    .item-price {
+                        float: right;
+                        margin-bottom: .5rem;
+                        margin-right: .5rem;
+                        margin-top: .5rem;
+                    }
+                `}</style>
+         </a>
+    )
+}
+export default MenuItem
--- src/pages/sides.js
+++ src/pages/sides.js
@@ -1,25 +1,15 @@
 import Layout from "../components/Layout"
 import React from "react"
+import MenuItems from "../components/MenuItem"
+import sidesInfo from "../../data/sides.json"
 
-const Menu = () => {
-
+const Sides = () => {
+    const MenuItemsComponents = sidesInfo.map(sidesInfo => (<MenuItems sidesInfo={sidesInfo}/>))
     return (
         <Layout>
             <h1 className="text-center mb-4">Side Orders</h1>
             <div className="sides-container">
-                <a href="#" className="side-item">
-                    <img src="./photos/AllSticks.jpg" alt="Track Town Pizza"/>
-                    <div className="description-box">
-                        <div className="item-price-description">
-                            <p className="item-description">Cheesy Garlic Sticks</p>
-                            <p className="item-price">$5.50</p>
-                        </div>
-                        <div className="item-price-description">
-                            <p className="item-description">Garlic Sticks</p>
-                            <p className="item-price">$5.00</p>
-                        </div>
-                    </div>
-                </a>
+                {MenuItemsComponents}
             </div>
 
             <style jsx>{`
@@ -28,42 +18,9 @@ const Menu = () => {
                     flex-flow: row wrap;
                     justify-content: center;
                 }
-
-                .side-item {
-                    min-width: 200px;
-                    width: 25%;
-                    margin-bottom:5%;
-                    margin-right: 2.5%;
-                    margin-left: 2.5%;
-                }
-                img {
-                    width: 100%;
-                    border-radius: 10px 10px 0px 0px;
-                }
-                .description-box {
-                    background: #FFEC65;
-                    border-radius: 0px 0px 10px 10px;
-                    overflow: auto;
-                    color: #000000
-                }
-                .item-price-description {
-                    overflow: auto;
-                }
-                .item-description {
-                    float: left;
-                    margin-bottom: .5rem;
-                    margin-left: .5rem;
-                    margin-top: .5rem;
-                }
-                .item-price {
-                    float: right;
-                    margin-bottom: .5rem;
-                    margin-right: .5rem;
-                    margin-top: .5rem;
-                }
             `}</style>
 
         </Layout>
     )
 }
-export default Menu
+export default Sides
