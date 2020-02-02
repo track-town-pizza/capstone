6c3e7acf0dc20618b825734676ce810e200ee1d1
hillkai <hillkai@oregonstate.edu>
2020-02-01 22:07:10 -0800

    rough version of items is working with breaking it up into components

--- src/pages/sides.js
+++ src/pages/sides.js
@@ -18,39 +18,6 @@ const Sides = () => {
                     flex-flow: row wrap;
                     justify-content: center;
                 }
-
-                .side-item {
-                    min-width: 200px;
-                    width: 25%;
-                    margin-bottom:5%;
-                    margin-right: 2.5%;
-                    margin-left: 2.5%;
-                }
-                img {
-                    width: 100%;
-                    border-radius: 10px 10px 0px 0px;
-                }
-                .description-box {
-                    background: #FFEC65;
-                    border-radius: 0px 0px 10px 10px;
-                    overflow: auto;
-                    color: #000000
-                }
-                .item-price-description {
-                    overflow: auto;
-                }
-                .item-description {
-                    float: left;
-                    margin-bottom: .5rem;
-                    margin-left: .5rem;
-                    margin-top: .5rem;
-                }
-                .item-price {
-                    float: right;
-                    margin-bottom: .5rem;
-                    margin-right: .5rem;
-                    margin-top: .5rem;
-                }
             `}</style>
 
         </Layout>
