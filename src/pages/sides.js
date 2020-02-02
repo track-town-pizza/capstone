83ef10a27fb86c9cd0accd29a71c26787e3df2ac
hillkai <hillkai@oregonstate.edu>
2020-02-01 16:25:09 -0800

    hardcoded side items works, images added, and json for side item info added

--- src/pages/sides.js
+++ src/pages/sides.js
@@ -1,10 +1,16 @@
 import Layout from "../components/Layout"
 import React from "react"
+<<<<<<< HEAD
 import MenuItems from "../components/MenuItem"
 import sidesInfo from "../../data/sides.json"
 
 const Sides = () => {
     const MenuItemsComponents = sidesInfo.map(sideInfo => (<MenuItems itemInfo={sideInfo} page="sides"/>))
+=======
+
+const Menu = () => {
+
+>>>>>>> hardcoded side items works, images added, and json for side item info added
     return (
         <Layout>
             <h1 className="text-center mb-4">Side Orders</h1>
