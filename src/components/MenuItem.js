db5a4b4d218e566a3aafd483bc0eac78291257a2
hillkai <hillkai@oregonstate.edu>
2020-02-02 17:56:40 -0800

    sauce page working and broken up into components

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,42 +2,29 @@ import React from "react"
 import ItemInfo from "./ItemInfo"
 
 const MenuItem = props => {
+    // console.log(props)
     return (
         <a href="#" className="side-item">
-            <img src={props.sidesInfo.imgLink} alt="Track Town Pizza"/>
-            <ItemInfo itemInformation={props.sidesInfo.sideInformation}/>
+            <img src={props.itemInfo.imgLink} alt="Track Town Pizza"/>
+            <ItemInfo itemInformation={props.itemInfo.information}/>
             <style jsx>{`
                     .side-item {
                         min-width: 200px;
                         width: 25%;
+                        min-height: 250px;
                         margin-bottom:5%;
                         margin-right: 2.5%;
                         margin-left: 2.5%;
+                        text-decoration: none;
                     }
                     img {
                         width: 100%;
                         border-radius: 10px 10px 0px 0px;
                     }
-                    .description-box {
-                        background: #FFEC65;
-                        border-radius: 0px 0px 10px 10px;
-                        overflow: auto;
-                        color: #000000
-                    }
-                    .item-price-description {
-                        overflow: auto;
-                    }
-                    .item-description {
-                        float: left;
-                        margin-bottom: .5rem;
-                        margin-left: .5rem;
-                        margin-top: .5rem;
-                    }
-                    .item-price {
-                        float: right;
-                        margin-bottom: .5rem;
-                        margin-right: .5rem;
-                        margin-top: .5rem;
+                    @media only screen and (max-width: 600px) {
+                        .side-item {
+                            min-height: 0px;
+                        }
                     }
                 `}</style>
          </a>
