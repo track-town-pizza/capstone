10b76d418546cec5127e0362278eed988a355822
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-02-04 20:53:28 -0800

    Refactor Login & Register pages to share components

---
+++ src/components/AccountFormHeader.js
@@ -0,0 +1,8 @@
+const AccountFormHeader = ({ title, subtitle }) => (
+	<span className="text-center dark-green-text">
+		<h2 className="text-uppercase page-title">{title}</h2>
+		<p>{subtitle}</p>
+	</span>
+)
+
+export default AccountFormHeader
