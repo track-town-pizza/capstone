da40e952968c85e8721f31670d97c3e13560d0ff
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-02-05 16:27:36 -0800

    Fix styling on Login page

--- src/components/AccountFormHeader.js
+++ src/components/AccountFormHeader.js
@@ -2,6 +2,11 @@ const AccountFormHeader = ({ title, subtitle }) => (
 	<span className="text-center dark-green-text">
 		<h2 className="text-uppercase page-title">{title}</h2>
 		<p>{subtitle}</p>
+		<style jsx>{`
+			.dark-green-text {
+				color: #094c3a;
+			}	
+		`}</style>
 	</span>
 )
 
