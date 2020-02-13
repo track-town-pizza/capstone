9e925a58d8c701cc245795550643440c015c5cd4
hillkai <hillkai@oregonstate.edu>
2020-02-12 17:08:26 -0800

    one more quick fix for links

--- src/pages/pizza.js
+++ src/pages/pizza.js
@@ -2,6 +2,8 @@ import Layout from "../components/Layout"
 import React from "react"
 import MenuItems from "../components/MenuItem"
 import pizzasInfo from "../../data/pizzas.json"
+import Link from "next/link"
+
 
 const Pizzas = () => {
     const MenuItemsComponents = pizzasInfo.map(pizzaInfo => (<MenuItems itemInfo={pizzaInfo} page="pizza"/>))
@@ -9,9 +11,13 @@ const Pizzas = () => {
         <Layout>
             <div className="text-center">
                 <h1 className="text-center mb-4">Track Town Pizzas</h1>
-                <a href="#" className="build-pizza-link">
-                    <p className="text-center mt-3">Build Your Own</p>
-                </a>
+                    <Link href="#">
+                        <div className="build-pizza-link">
+                            <a>
+                                <p className="text-center mt-3">Build Your Own</p>
+                            </a>
+                        </div>
+                    </Link>
                 <h3 className="text-center mb-4">Or Check Out Our Delicious Menu Pizzas</h3>
             </div>
             <div className="pizzas-container">
@@ -38,6 +44,10 @@ const Pizzas = () => {
                     margin-bottom: 1.5rem;
                     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
                 }
+                .build-pizza-link:hover {
+                    cursor: pointer;
+                }
+
             `}</style>
 
         </Layout>
