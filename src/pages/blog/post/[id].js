9d6f105452239cd675eeddcd6f90c0c9995162f2
AidenJN <nelsonai@oregonstate.edu>
2020-02-15 19:47:48 -0800

    Dynamic linking now works with blogs

--- src/pages/blog/post/[id].js
+++ src/pages/blog/post/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import Layout from "../../components/Layout"
+import Layout from "../../../components/Layout"
 
 const Post = () => {
 	const router = useRouter()
