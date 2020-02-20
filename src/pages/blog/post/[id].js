08ce08675b6a881c58556b24f35a63488b040d48
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-02-19 18:22:10 -0800

    Fix merge conflicts for blog files

--- src/pages/blog/post/[id].js
+++ src/pages/blog/post/[id].js
@@ -1,14 +1,29 @@
 import { useRouter } from "next/router"
-import Layout from "../../components/Layout"
+import Layout from "../../../components/Layout"
+import postData from "../../../../data/PostData.json" // will be removed with backend implementation
+import SinglePost from "../../../components/SinglePost.js"
 
 const Post = () => {
 	const router = useRouter()
 
-	return (
-		<Layout>
-			<p>Post</p>
-		</Layout>
-	)
+    // !!!WARNING!!!
+    // Data fetching is currently reliant on the fact that post id's match with array positions.
+    // Will have to fetch based on key when using MongoDB
+
+    if (postData.posts[router.query.id] != undefined) {
+        const postNum = router.query.id
+        return (
+            <Layout>
+                <SinglePost post={postData.posts[postNum]} key={postNum}/>
+            </Layout>
+        )
+    } else {
+        return (
+            <Layout>
+                <p>Could not display post :(</p>
+            </Layout>
+        )
+    }
 }
 
 export default Post
