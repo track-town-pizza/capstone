12e19288f9069b4a85deae70d851b8f89af680d6
AidenJN <nelsonai@oregonstate.edu>
2020-02-26 14:29:27 -0800

    Removed redundant post directory

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,14 +1,14 @@
 import { useRouter } from "next/router"
-import Layout from "../../../components/Layout"
-import postData from "../../../../data/PostData.json" // will be removed with backend implementation
-import SinglePost from "../../../components/SinglePost.js"
+import Layout from "../../components/Layout"
+import postData from "../../../data/PostData.json" // will be removed with backend implementation
+import SinglePost from "../../components/SinglePost.js"
 
 const Post = () => {
 	const router = useRouter()
 
     // !!!WARNING!!!
     // Data fetching is currently reliant on the fact that post id's match with array positions.
-    // Will have to fetch based on key when using MongoDB
+    // Will have to fetch based on date when using MongoDB
 
     if (postData.posts[router.query.id] != undefined) {
         const postNum = router.query.id
