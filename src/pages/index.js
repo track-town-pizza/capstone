70610a93a6ca092b2a87ce95ae667a80e226897e
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-02-25 20:41:42 -0800

    Refactor Home page to be mobile-friendly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,165 +1,303 @@
 import React from "react"
+import Link from "next/link"
 import Layout from "../components/Layout"
 
+import info from '../../data/info.json'
 import events from "../../data/events.json"
+import postData from "../../data/PostData.json"
 import EventListing from "../components/EventListing"
 
-const Index = () => (
-	<Layout>
-		<div className="home-order-photo">
-			{/* Photo and order text overlay go here */}
-			<div className="home-photo">
-				<img src="/photos/track_town_front_cropped.jpg" alt="Track Town Front" />
-			</div>
-			<div className="order-overlay">
-				<div className="order-text">
-					<p>
-						Welcome to Eugene's best pizza!
-						<br /><br />
-						Call us:
-						<br />
-						541-284-8484
-					</p>
-					<a className="btn btn-yellow">Order Online!</a>
+const Index = () => {
+	const post = postData.posts[0];
+
+	return (
+		<Layout>
+			<div className="home-order-photo home-font">
+				<div className="home-photo">
+					<img src="/photos/track_town_front_cropped.jpg" alt="Track Town Front" />
+				</div>
+				<div className="order-overlay">
+					<div className="order-text">
+						<p>
+							Welcome to Eugene's best pizza!
+							<span className="responsive-break"><br /><br /></span>
+							<span className="responsive-spacing">&nbsp;</span>
+							Call us:
+							<br />
+							<span className="responsive-phone">{info.phone}</span>
+						</p>
+						<Link href={info.onlineOrderLink}>
+							<a className="btn btn-yellow btn-responsive">Order Online!</a>
+						</Link>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div className="info-containers">
-			<div className="yellow-container extra-padding">
-				<h3>We're open all week! Come visit us:</h3>
-				<br /><br />
-				<h4>Sun - Thur</h4>
-				<h4 className="no-margin-top grey-font">11 am - 12 pm</h4>
-				<h4>Fri - Sat</h4>
-				<h4 className="no-margin-top grey-font">11 am - 1 am</h4>
+			<div className="info-containers home-font">
+				<div className="yellow-container extra-padding">
+					<h3>We're open all week! Come visit us:</h3>
+					<br />
+					<div className="hours-container">
+						<h4>Sun - Thur</h4>
+						<h4 className="no-margin-top grey-font">{info.openHourRegular} {info.openHourRegularPeriod} - {info.closeHourRegular} {info.closeHourRegularPeriod}</h4>
+					</div>
+					<br />
+					<div className="hours-container">
+						<h4>Fri - Sat</h4>
+						<h4 className="no-margin-top grey-font">{info.openHourFriSat} {info.openHourFriSatPeriod} - {info.closeHourFriSat} {info.closeHourFriSatPeriod}</h4>
+					</div>
+				</div>
+				<div className="yellow-container">
+					<h3>Events</h3>
+					<table className="table-sm table-font">
+						<tbody>
+							{events.map(event => (
+								<EventListing key={event.id} {...event} />
+							))}
+						</tbody>
+					</table>
+				</div>
+				<div className="yellow-container extra-padding">
+					<h4>Located at {info.address}</h4>
+					<br />
+					<h4>Across from Matthew Knight Arena</h4>
+					<img src={info.matthewKnightImgLink} alt="Matthew Knight Arena Front" />
+				</div>
 			</div>
-			<div className="yellow-container">
-				<h3>Events</h3>
-				<table className="table-sm table-font">
-					<tbody>
-						{events.map(event => (
-							<EventListing key={event.id} {...event} />
-						))}
-					</tbody>
-				</table>
+			<div className="blog-container home-font">
+				<div className="blog-left-column">
+					<h3>{post.title}</h3>
+					<img src={post.imageLink} />
+				</div>
+				<div className="blog-right-column">
+					<p>{post.content}</p>
+				</div>
 			</div>
-			<div className="yellow-container">
-				Location
-			</div>
-		</div>
-		<div className="w-100">
-			{/* Most recent blog post goes here */}
-		</div>
-		<style jsx>{`
-			.home-order-photo {
-				position: relative;
-				display: flex;
-				align-items: center;
-			}
-
-			.home-photo > img {
-				width: 100%;
-			}
-
-			.home-order-photo > .order-overlay {
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 25%;
-				height: 100%;
-				background-color: rgba(0, 112, 48, 0.6);
-			}
-
-			.order-text {
-				width: 100%;
-				height: 100%;
-				padding: 10px;
-				display: flex;
-				flex-wrap: wrap;
-				align-items: center;
-				justify-content: center;
-				text-align: center;
-				font-size: 30px;
-				color: white;
-			}
-
-			@media only screen and (max-width: 1300px) {
-				.order-text {
-					font-size: 25px;
+			<style jsx>{`
+				.home-font {
+					font-family: 'Open Sans Condensed', sans-serif;
 				}
-			}
 
-			@media only screen and (max-width: 1100px) {
+				.home-order-photo {
+					position: relative;
+					display: flex;
+					align-items: center;
+					border-radius: 5px;
+				}
+
+				.home-photo > img {
+					width: 100%;
+					border-radius: 5px;
+				}
+
+				.home-order-photo > .order-overlay {
+					position: absolute;
+					top: 0;
+					left: 0;
+					width: 25%;
+					height: 100%;
+					border-top-left-radius: 2%;
+					border-bottom-left-radius: 2%;
+					background-color: rgba(0, 112, 48, 0.6);
+				}
+
+				@media only screen and (max-width: 1100px) {
+					.order-overlay {
+						width: 30% !important;
+					}
+
+					@media only screen and (max-width: 900px) {
+						.home-order-photo {
+							position: static !important;
+							display: flex !important;
+							flex-direction: column;
+						}
+
+						.order-overlay {
+							position: static !important;
+							width: 100% !important;
+							margin-top: 20px;
+							padding: 10px 0;
+							background-color: rgba(0, 112, 48, 1) !important;
+							border-radius: 5px !important;
+						}
+
+						.responsive-break {
+							display: none;
+						}
+
+						.responsive-spacing {
+							display: inline !important;
+						}
+
+						.responsive-phone {
+							font-size: 30px;
+						}
+					}
+				}
+
 				.order-text {
+					width: 100%;
+					height: 100%;
+					padding: 10px;
+					display: flex;
+					flex-wrap: wrap;
+					align-items: center;
+					justify-content: center;
+					text-align: center;
+					font-size: 30px;
+					color: white;
+				}
+
+				.responsive-spacing {
+					display: none;
+				}
+
+				@media only screen and (max-width: 1300px) {
+					.order-text {
+						font-size: 25px;
+					}
+
+					@media only screen and (max-width: 1100px) {
+						.order-text {
+							font-size: 20px;
+						}
+					}
+				}
+
+				.order-text > a {
+					color: #007030;
+				}
+
+				.btn-yellow {
+					width: 75%;
+					border: 1px solid #ccb400;
+					background-color: #ffec65;
 					font-size: 20px;
 				}
-			}
 
-			@media only screen and (max-width: 1000px) {
-				.order-overlay {
-					width: 30%;
+				.btn-yellow:hover {
+					transition: 0.5s;
+					background-color: #e6cb00;
+					color: white;
 				}
-			}
 
-			@media only screen and (max-width: 900px) {
-				.order-overlay {
-					width: 50%;
+				.btn-responsive {
+					white-space: nowrap;
 				}
-			}
 
-			.order-text > a {
-				color: #007030;
-			}
+				.info-containers {
+					margin-top: 20px;
+					display: flex;
+					flex-wrap: wrap;
+					flex-grow: 1;
+					justify-content: space-between;
+					align-items: center;
+				}
 
-			.btn-yellow {
-				width: 75%;
-				border: 1px solid #ccb400;
-				background-color: #ffec65;
-				font-size: 20px;
-			}
+				.yellow-container {
+					width: 32%;
+					max-width: 370px;
+					max-height: 345px;
+					height: 345px;
+					margin-bottom: 2%;
+					padding: 20px;
+					border: 1px solid #ffe100;
+					border-radius: 2%;
+					background-color: #ffec65;
+					overflow: hidden;
+					text-align: center;
+				}
 
-			.btn-yellow:hover {
-				transition: 0.5s;
-				background-color: #e6cb00;
-				color: white;
-			}
+				.yellow-container img {
+					max-width: 200px;
+					width: auto;
+					margin: 15px auto 0 auto;
+					border: 1px solid #ffe100;
+					border-radius: 2px;
+				}
 
-			.info-containers {
-				margin-top: 20px;
-				display: flex;
-				flex-wrap: wrap;
-				flex-grow: 1;
-				justify-content: space-between;
-				align-items: center;
-			}
+				@media only screen and (max-width: 900px) {
+					.info-containers {
+						flex-wrap: wrap;
+						justify-content: center;
+						padding: 0;
+					}
 
-			.yellow-container {
-				width: 32%;
-				max-width: 370px;
-				height: 325px;
-				margin-bottom: 5%;
-				padding: 20px;
-				background-color: #ffec65;
-				text-align: center;
-			}
+					.yellow-container {
+						max-width: 100%;
+						width: 100%;
+						height: auto;
+						margin-top: 0;
+						border-radius: 5px;
+					}
 
-			.extra-padding {
-				padding: 40px;
-			}
+					.extra-padding {
+						padding: 10px 0 !important;
+					}
+				}
 
-			.no-margin-top h4 {
-				margin-top: 0;
-			}
+				.extra-padding {
+					padding: 35px;
+				}
 
-			.grey-font {
-				color: gray;
-			}
+				.no-margin-top h4 {
+					margin-top: 0;
+				}
 
-			.table-font {
-				font-size: 15px;
-			}
-		`}</style>
-	</Layout>
-)
+				.grey-font {
+					color: gray;
+				}
+
+				.table-font {
+					font-size: 16px;
+				}
+
+				.blog-container {
+					margin-bottom: 5%;
+					height: auto;
+					border: 1px solid #004d21;
+					border-radius: 5px;
+					padding: 20px;
+					display: flex;
+					background-color: #007030;
+					color: white;
+				}
+
+				.blog-left-column {
+					width: 40%;
+					padding: 10px;
+				}
+
+				.blog-left-column > img {
+					margin-top: 2%;
+					max-width: 100%;
+					border: 1px solid darkgreen;
+					border-radius: 5px;
+				}
+
+				.blog-right-column {
+					width: 60%;
+					padding: 20px;
+					display: flex;
+					align-items: center;
+				}
+
+				@media only screen and (max-width: 900px) {
+					.blog-container {
+						flex-direction: column;
+					}
+
+					.blog-left-column, .blog-right-column {
+						width: auto;
+					}
+
+					.blog-right-column {
+						padding: 20px 10px 0 10px;
+					}
+				}
+			`}</style>
+		</Layout>
+	);
+}
 
 export default Index
