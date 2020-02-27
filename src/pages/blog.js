74ad76793bfde74ef30b218f87d3637451b972db
AidenJN <nelsonai@oregonstate.edu>
2020-02-26 17:55:12 -0800

    commented out some stuff

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,7 +5,24 @@ import SinglePost from "../components/SinglePost"
 
 const postRenderLimit = 3;
 
+/*
+function getPostSet(pageNumber) {
+    postSet = []
+
+}
+*/
+
 const Blog = props => (
+
+    /*
+    const [blogState, setBlog] = useState({
+        page: 0,
+
+
+    })
+    */
+
+
 	<Layout>
 		{postData.posts.map(post => (
             <SinglePost post={post} key={post.id}/>
