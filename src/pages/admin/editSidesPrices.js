67cbcb2aa860f8e3f88d9304f366f9a294d49a55
hillkai <hillkai@oregonstate.edu>
2020-03-20 22:17:31 -0700

    basics to update the side order prices

--- src/pages/admin/editSidesPrices.js
+++ src/pages/admin/editSidesPrices.js
@@ -2,6 +2,9 @@ import React, {useState} from "react"
 import allSidesInfo from "../../../data/sides.json"
 import Layout from "../../components/Layout"
 import EditFoodItem from "../../components/admin/EditFoodItem"
+import SubmitButton from "../../components/admin/SubmitPricesBtn"
+const MONEY_PATTERN = /^\$(\d{1,3}(\,\d{3})*|(\d+))(\.[0-9]{2})$/
+
 
 function parseJsonToUsableObj() {
     let sidesInfo = []
@@ -31,12 +34,30 @@ const editSidesPrices = () => {
         )))
     }
 
+    function onClick(event) {
+        const { type } = event
+        let success = true
+        if(type === 'click') {
+            for(const side of sides) {
+                if(!MONEY_PATTERN.test(side.price)) {
+                    success = false
+                    alert(side.description + " price was not done correctly. It must be in the form $X.XX or $XX.XX. Please fix it before this form can be submitted")
+                }
+            }
+            if(success) {
+                alert("The prices have been updated")
+                // combine old and new information to update the database
+            }
+        }
+    }
+
     return (
         <Layout>
             <h2 className="text-center">Edit Side Orders' Prices</h2>
             <div className="text-center">
                {EditSidesItems}
             </div>
+            <SubmitButton words="Submit Side Prices" onClick={onClick} />
             <style jsx>{`
                 .box {
                     text-align:center;
