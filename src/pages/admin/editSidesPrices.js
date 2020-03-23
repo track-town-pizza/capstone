ef1a478d5511f62651cf97bc12391756cfac0387
hillkai <hillkai@oregonstate.edu>
2020-03-23 15:28:40 -0700

    added comment and fixed a small notation error

--- src/pages/admin/editSidesPrices.js
+++ src/pages/admin/editSidesPrices.js
@@ -3,9 +3,12 @@ import allSidesInfo from "../../../data/sides.json"
 import Layout from "../../components/Layout"
 import EditFoodItem from "../../components/admin/EditFoodItem"
 import SubmitButton from "../../components/admin/SubmitPricesBtn"
+// This is a regular expression that tells is used for checking that the 
+// new price is in the correct format
 const MONEY_PATTERN = /^\$(\d{1,3}(\,\d{3})*|(\d+))(\.[0-9]{2})$/
 
-
+// This function takes the original data and returns an array of data that 
+// can be udpated
 function parseJsonToUsableObj() {
     let sidesInfo = []
     for(let side of allSidesInfo) {
@@ -23,6 +26,7 @@ const editSidesPrices = () => {
     const EditSidesItems = []
     EditSidesItems.push(sides.map(item => <EditFoodItem id={item.description} name={item.description} defaultValue={item.price} onChange={onChange}/>))
 
+    // This function updates the text that the user sees as they change the price
     function onChange(event) {
         const { id } = event.target
         setSides( sides.map(currSide => (
@@ -34,31 +38,38 @@ const editSidesPrices = () => {
         )))
     }
 
+    // This function controls what happens when the user hits the submit button
     function onClick(event) {
         const { type } = event
         let success = true
         if(type === 'click') {
             for(const side of sides) {
+                // if the user messed up the format for the price, they must fix it 
+                // before the prices can be updated
                 if(!MONEY_PATTERN.test(side.price)) {
                     success = false
                     alert(side.description + " price was not done correctly. It must be in the form $X.XX or $XX.XX. Please fix it before this form can be submitted")
                 }
             }
+            // the all the elements are in the correct format, the prices can be updated
             if(success) {
                 alert("The prices have been updated")
+                // A new object is created that matches the original format of the object for the database
+                // The new information is merged with the unchanged information
                 const newSidesPrices = []
                 let iter = 0
                 for (const sideInfo of allSidesInfo) {
-                    const obj = new Object()
+                    const obj = {}
                     obj.key = sideInfo.key
                     obj.imgLink = sideInfo.imgLink
-                    obj.information = new Array()
+                    obj.information = []
                     for(let i=iter; i<iter+sideInfo.information.length; i++) {
                         obj.information.push(sides[i])
                     }
                     iter += sideInfo.information.length
                     newSidesPrices.push(obj)
                 }
+                console.log(newSidesPrices)
                 // push newSidesPrices into the db as it is the updated information
             }
         }
