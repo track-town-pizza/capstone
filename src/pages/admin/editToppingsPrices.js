93c3e74551a36d1bf7c32b3ec32ab85ed3d2755f
hillkai <hillkai@oregonstate.edu>
2020-03-25 16:58:23 -0700

    data is parsed from original json and onChange works

--- src/pages/admin/editToppingsPrices.js
+++ src/pages/admin/editToppingsPrices.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react"
 import allToppingsInfo from "../../../data/prices.json"
+import pizzaInfo from "../../../data/pizzaInfo.json"
 import Layout from "../../components/Layout"
 import EditFoodItem from "../../components/admin/EditFoodItem"
 import SubmitButton from "../../components/admin/SubmitPricesBtn"
@@ -7,81 +8,114 @@ import SubmitButton from "../../components/admin/SubmitPricesBtn"
 // new price is in the correct format
 const MONEY_PATTERN = /^\$(\d{1,3}(\,\d{3})*|(\d+))(\.[0-9]{2})$/
 
-// This function takes the original data and returns an array of data that 
+// This function takes the original data and modifies two arrays of data that 
 // can be udpated
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-// not exactly sure what to do, will probably assume that there are always 5 sizes and work from there
-function parseJsonToUsableObj() {
-    let toppingsInfo = []
-    for(let topping of allToppingsInfo) {
-        sidesInfo = sidesInfo.concat(side.information.map(item => { 
-            const { description, price } = item
-            return { description, price}
-        }))
+function parseJsonToUsableObj(toppingsPriceInfo, sizePriceInfo) {
+    for(const size of pizzaInfo.sizes) {
+        sizePriceInfo.push({prices: allToppingsInfo[size], description: size})
     }
-    return sidesInfo
+    for(const toppingStr of pizzaInfo.toppings.meats) {
+        const topping = toppingStr.replace(/ /g, "_")
+        toppingsPriceInfo.push({prices: allToppingsInfo[topping], description: topping})
+    }
+    for(const toppingStr of pizzaInfo.toppings.others) {
+        const topping = toppingStr.replace(/ /g, "_")
+        toppingsPriceInfo.push({prices: allToppingsInfo[topping], description: topping})
+    }
 }
 
-const editSidesPrices = () => {
-    const sidesInfo = parseJsonToUsableObj()
-    const [ sides, setSides ] = useState(sidesInfo)
-    const EditSidesItems = []
-    EditSidesItems.push(sides.map(item => <EditFoodItem id={item.description} name={item.description} defaultValue={item.price} onChange={onChange}/>))
+const editToppingsPrices = () => {
+    const toppingsPriceInfo = []
+    const sizePriceInfo = []
+    parseJsonToUsableObj(toppingsPriceInfo, sizePriceInfo)
+    const [ toppings, setToppings ] = useState(toppingsPriceInfo)
+    const [ sizes, setSizes ] = useState(sizePriceInfo)
+
+    // use state to make the elements the user will see
+    const EditToppingItems = []
+    const EditPizzaItems = []
+    for(const topping of toppings) {
+        const toppingStr = topping.description.replace(/_/g, " ")
+        EditToppingItems.push(<h4>{toppingStr}</h4>)
+        for(const size of sizes) {
+            EditToppingItems.push(<EditFoodItem id={topping.description+"*"+size.description} name={size.description} defaultValue={topping.prices[size.description]} onChange={onChange}/>)
+        }
+    }
+    for(const size of sizes) {
+        EditPizzaItems.push(<EditFoodItem id={size.description} name={size.description} defaultValue={size.prices} onChange={onChange}/>)
+    }
 
     // This function updates the text that the user sees as they change the price
     function onChange(event) {
         const { id } = event.target
-        setSides( sides.map(currSide => (
-            id === currSide.description ? 
+        // sizes and toppings must be done separately because they are two different state calls
+        if(id === "Individual" || id === "Small" || id === "Medium" || id === "Large" || id === "Giant") {
+            setSizes(sizes.map(currSize => (
+            id === currSize.description ? 
             {
-                ...currSide,
-                price: event.target.value
-            } : {...currSide}
-        )))
+                ...currSize,
+                prices: event.target.value
+            } : {...currSize}
+            )))
+        }
+        else {
+            const newid = id.split("*")
+            setToppings(toppings.map(currTopping => (
+                newid[0] === currTopping.description.replace(/ /g, "_") ?
+                {
+                    ...currTopping,
+                    prices: {
+                        ...currTopping.prices,
+                        [newid[1]]: event.target.value
+                    }
+                } : {...currTopping}
+                )))
+        }
     }
 
     // This function controls what happens when the user hits the submit button
     function onClick(event) {
-        const { type } = event
-        let success = true
-        if(type === 'click') {
-            for(const side of sides) {
-                // if the user messed up the format for the price, they must fix it 
-                // before the prices can be updated
-                if(!MONEY_PATTERN.test(side.price)) {
-                    success = false
-                    alert(side.description + " price was not done correctly. It must be in the form $X.XX or $XX.XX. Please fix it before this form can be submitted")
-                }
-            }
-            // the all the elements are in the correct format, the prices can be updated
-            if(success) {
-                alert("The prices have been updated")
-                // A new object is created that matches the original format of the object for the database
-                // The new information is merged with the unchanged information
-                const newSidesInfo = JSON.parse(JSON.stringify(allSidesInfo))
-                let iter = 0
-                for (const sideInfo of newSidesInfo) {
-                    const len = sideInfo.information.length
-                    sideInfo.information = []
-                    for(let i=iter; i<iter+len; i++) {
-                        sideInfo.information.push(sides[i])
-                    }
-                    iter += sideInfo.information.length
-                }
-                console.log(newSidesInfo)
-                // push newSidesPrices into the db as it is the updated information
-            }
-        }
+        // const { type } = event
+        // let success = true
+        // if(type === 'click') {
+        //     for(const side of sides) {
+        //         // if the user messed up the format for the price, they must fix it 
+        //         // before the prices can be updated
+        //         if(!MONEY_PATTERN.test(side.price)) {
+        //             success = false
+        //             alert(side.description + " price was not done correctly. It must be in the form $X.XX or $XX.XX. Please fix it before this form can be submitted")
+        //         }
+        //     }
+        //     // the all the elements are in the correct format, the prices can be updated
+        //     if(success) {
+        //         alert("The prices have been updated")
+        //         // A new object is created that matches the original format of the object for the database
+        //         // The new information is merged with the unchanged information
+        //         const newSidesInfo = JSON.parse(JSON.stringify(allSidesInfo))
+        //         let iter = 0
+        //         for (const sideInfo of newSidesInfo) {
+        //             const len = sideInfo.information.length
+        //             sideInfo.information = []
+        //             for(let i=iter; i<iter+len; i++) {
+        //                 sideInfo.information.push(sides[i])
+        //             }
+        //             iter += sideInfo.information.length
+        //         }
+        //         console.log(newSidesInfo)
+        //         // push newSidesPrices into the db as it is the updated information
+        //     }
+        // }
     }
 
     return (
         <Layout>
-            <h2 className="text-center">Edit Side Orders' Prices</h2>
+            <h2 className="text-center">Edit Topping Prices</h2>
             <div className="text-center">
-               {EditSidesItems}
+               {EditToppingItems}
+                <h4>Pizza Sizes</h4>
+                {EditPizzaItems}
             </div>
-            <SubmitButton words="Submit Side Prices" onClick={onClick} />
+            <SubmitButton words="Submit Topping Prices" onClick={onClick} />
             <style jsx>{`
                 .box {
                     text-align:center;
@@ -92,4 +126,4 @@ const editSidesPrices = () => {
         </Layout>
     )
 }
-export default editSidesPrices
+export default editToppingsPrices
