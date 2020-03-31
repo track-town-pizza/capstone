67b34352da01af348cb7246d70b9da79a4b81a9a
hillkai <hillkai@oregonstate.edu>
2020-03-31 15:58:47 -0700

    created merchandise page from sides page

---
+++ src/pages/admin/editMerchandisePrices.js
@@ -0,0 +1,92 @@
+import React, {useState} from "react"
+import allMerchInfo from "../../../data/merchandise.json"
+import Layout from "../../components/Layout"
+import EditFoodItem from "../../components/admin/EditFoodItem"
+import SubmitButton from "../../components/admin/SubmitPricesBtn"
+// This is a regular expression that tells is used for checking that the 
+// new price is in the correct format
+const MONEY_PATTERN = /^\$(\d{1,3}(\,\d{3})*|(\d+))(\.[0-9]{2})$/
+
+// This function takes the original data and returns an array of data that 
+// can be udpated
+function parseJsonToUsableObj() {
+    let merchInfo = []
+    for(let merch of allMerchInfo) {
+        merchInfo = merchInfo.concat(merch.information.map(item => { 
+            const { description, price } = item
+            return { description, price}
+        }))
+    }
+    return merchInfo
+}
+
+const editMerchandisePrices = () => {
+    const merchInfo = parseJsonToUsableObj()
+    const [ merchandise, setMerchandise ] = useState(merchInfo)
+    const EditSidesItems = []
+    EditSidesItems.push(merchandise.map(item => <EditFoodItem id={item.description} name={item.description} defaultValue={item.price} onChange={onChange}/>))
+
+    // This function updates the text that the user sees as they change the price
+    function onChange(event) {
+        const { id } = event.target
+        setMerchandise( merchandise.map(currMerch => (
+            id === currMerch.description ? 
+            {
+                ...currMerch,
+                price: event.target.value
+            } : {...currMerch}
+        )))
+    }
+
+    // This function controls what happens when the user hits the submit button
+    function onClick(event) {
+        const { type } = event
+        let success = true
+        if(type === 'click') {
+            for(const merch of merchandise) {
+                // if the user messed up the format for the price, they must fix it 
+                // before the prices can be updated
+                if(!MONEY_PATTERN.test(merch.price)) {
+                    success = false
+                    alert(merch.description + " price was not done correctly. It must be in the form $X.XX or $XX.XX. Please fix it before this form can be submitted")
+                }
+            }
+            // the all the elements are in the correct format, the prices can be updated
+            if(success) {
+                alert("The prices have been updated")
+                // A new object is created that matches the original format of the object for the database
+                // The new information is merged with the unchanged information
+                const newMerchandiseInfo = JSON.parse(JSON.stringify(allMerchInfo))
+                let iter = 0
+                for (const sideInfo of newMerchandiseInfo) {
+                    const len = sideInfo.information.length
+                    sideInfo.information = []
+                    for(let i=iter; i<iter+len; i++) {
+                        sideInfo.information.push(merchandise[i])
+                    }
+                    iter += sideInfo.information.length
+                }
+                console.log(newMerchandiseInfo)
+                // push newSidesPrices into the db as it is the updated information
+            }
+        }
+    }
+
+    return (
+        <Layout>
+            <h2 className="text-center">Edit Side Orders' Prices</h2>
+            <div className="text-center">
+               {EditSidesItems}
+            </div>
+            <SubmitButton words="Submit Side Prices" onClick={onClick} />
+            <style jsx>{`
+                .box {
+                    text-align:center;
+                }
+	
+			`}</style>
+
+        </Layout>
+    )
+}
+export default editMerchandisePrices
