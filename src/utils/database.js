2c1db13c81f6605d2077b583a5219353525c7d55
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-04-01 13:50:26 -0700

    Implement database middleware

---
+++ src/utils/database.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb"
+import nextConnect from "next-connect"
+
+const client = new MongoClient(process.env.MONGODB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+async function database(req, res, next) {
+	if (!client.isConnected()) {
+		await client.connect()
+	}
+
+	req.dbClient = client
+	req.db = client.db("TrackTownPizzaDatabase")
+
+	return next()
+}
+
+const middleware = nextConnect()
+
+middleware.use(database)
+
+export default middleware
