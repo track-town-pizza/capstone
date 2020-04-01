964749734081f11deaf0158a5113081110afd449
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-04-01 13:50:18 -0700

    Update Next config for environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const API_KEY = process.env.API_KEY
-module.exports = {
-  env: {API_KEY}
-}
+const nextEnv = require("next-env")
+const dotEnvLoad = require("dotenv-load")
+
+dotEnvLoad()
+
+const withNextEnv = nextEnv()
+
+module.exports = withNextEnv({
+  env: {
+    "MONGODB_URL": process.env.MONGODB_URL,
+    "URL_ROOT": process.env.URL_ROOT,
+    "API_KEY": process.env.API_KEY,
+    "PORT": process.env.PORT
+  }
+})
