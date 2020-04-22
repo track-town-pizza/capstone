aade84238a2e02566ecd65bb1c27f57b4037df0a
AidenJN <nelsonai@oregonstate.edu>
2020-04-21 17:43:17 -0700

    Phone number clickable from pizza builder page

--- src/components/menu/NotOnlineOrdering.js
+++ src/components/menu/NotOnlineOrdering.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Link from "next/link"
 
+function removeDashes(number) {
+    return number.replace('-', '')
+}
+
 const NotOnlineOrdering = (props) => {
     return (
         <div className="text-center">
             <h3>This is not online ordering</h3>
             <div className="mr-2 ml-2">
-                <p className="d-inline">{"If you would like to place an order please call " + props.phoneNumber + " or click "}</p>
+                <p className="d-inline">If you would like to place an order please call  <a href={"tel:+" + removeDashes(props.phoneNumber)}> {props.phoneNumber}</a>  or click </p>
                 <Link href={props.onlineOrderingLink}>
                     <a className="d-inline text-success">here</a>
                 </Link>
