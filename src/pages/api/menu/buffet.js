a514b01b0ba46fa788ce6cb78739594351cdaf7a
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-04-21 20:11:31 -0700

    Connected Edit Buffet page to DB

--- src/pages/api/menu/buffet.js
+++ src/pages/api/menu/buffet.js
@@ -10,4 +10,24 @@ handler.get(async (req, res) => {
 	res.json(doc)
 })
 
+handler.post(async (req, res) => {
+	const { buffet } = req.body
+	
+	// Remove _id attribute to prevent attempts to update it in DB
+	delete buffet["_id"]
+
+	try {
+		await req.db.collection("buffet").update(
+			{ key: "Buffet Prices" },
+			{ $set: buffet },
+			{ upsert: true }
+		)
+		res.status(200).json({ message: "OK" })
+	} catch (err) {
+		// Return error with 500 status code if update fails
+		console.log("== Error:", err)
+		res.status(500).json({ err })
+	}
+})
+
 export default handler
