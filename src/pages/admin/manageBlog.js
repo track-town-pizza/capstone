b1be88e9c944c601d52baac611bba0c7e2de254b
AidenJN <nelsonai@oregonstate.edu>
2020-05-01 15:33:44 -0700

    Created new post page

--- src/pages/admin/manageBlog.js
+++ src/pages/admin/manageBlog.js
@@ -7,29 +7,10 @@ import GeneralButton from "../../components/admin/GeneralBtn"
 import BlogManagementList from "../../components/admin/BlogManagementList"
 
 const ManageBlog = () => {
-
-    //let postInfoList = ["dog", "log", "bog"]
-
-
-     // This function controls what happens when the user hits the delete button
-    /*
-    function onClick(event) {
-        const { name, type } = event.target
-        //console.log("type: ", type)
-        if(type === 'submit' || type === 'click') {
-            if (confirm("Are you sure you want to delete this post? Post ID: " + toString(name))) {
-                // Remove post from database
-                alert("Post deleted.")
-            }
-        }
-    }
-    */
-
    return (
         <Layout>
             <h2 className="text-center">Manage Blog Posts</h2>
             <div className="text-center">
-       {/*<BlogManagementList removePostFunc={onClick} postInfos={postInfoList}/>*/}
                 <BlogManagementList/>
             </div>
             <div className="d-flex flex-row justify-content-between">
@@ -37,7 +18,7 @@ const ManageBlog = () => {
                     <ManagementHubButton/>
                 </div>
                 <div className="d-inline p-2">
-                    <GeneralButton words="Create New Post" link="/.."/>
+                    <GeneralButton words="Create New Post" link="./createNewPost"/>
                 </div>
             </div>
 			<style jsx>{`
