26dcbdaa6d9611e6074b146af6e6c4d75aa9da2f
AidenJN <nelsonai@oregonstate.edu>
2020-05-01 16:19:15 -0700

    Moved click functions to individual list components

--- src/components/admin/BlogManagementList.js
+++ src/components/admin/BlogManagementList.js
@@ -9,29 +9,14 @@ const BlogManagementList = props => {
     let postInfoList = ["dog", "log", "bog"]
 
 
-     // This function controls what happens when the user hits the delete button
-    function onClick(event) {
-        const { name, type } = event.target
-        //console.log("type: ", type)
-        if(type === 'submit' || type === 'click') {
-            if (confirm("Are you sure you want to delete this post? Post ID: " + toString(name))) {
-                // Remove post from database
-                alert("Post deleted.")
-            }
-        }
-    }
-
-
     return (
         <div>
             <ul className="list-group">
                 {postInfoList.map(postInfo => (
-                    <BlogManagementListItem removePostFunc={onClick} pname={postInfo} name={postInfo} />
+                    <BlogManagementListItem pname={postInfo} name={postInfo} />
                 ))}
 
             </ul>
-
-            <button onClick={props.removePostFunc} name={props.pname}/>
 
             <style jsx>{`
 
--- src/components/admin/BlogManagementListItem.js
+++ src/components/admin/BlogManagementListItem.js
@@ -1,13 +1,56 @@
 import React from "react"
 
 const BlogManagementListItem = props => {
+
+
+     // This function controls what happens when the user hits the delete button
+    function onClickDelete(event) {
+        const { name, type } = event.target
+        if(type === 'submit' || type === 'click') {
+            if (confirm("Are you sure you want to delete this post? Post ID: " + toString(name))) {
+                // Remove post from database
+                alert("Post deleted.")
+            }
+        }
+    }
+
+    function onClickEdit(event) {
+        const { name, type } = event.target
+        if(type === 'submit' || type === 'click') {
+            // Remove post from database
+        }
+    }
+
     return (
+
         <div>
-            <li onClick={props.removePostFunc} name={props.name}>{props.pname}</li>
+            <li name={props.name}>
+                <span className="datePart">10/20/2018</span>
+                <span className="titlePart">{props.pname}</span>
+                <span className="editButtonPart"><button onClick={onClickEdit}>Edit</button></span>
+                <span className="deleteButtonPart" button onClick={onClickDelete}><button>Delete</button></span>
+            </li>
             <style jsx>{`
                 li {
                     padding: 10px;
+                    margin-right: 10px;
+                    margin-left: 10px;
+                    border-style: solid;
+                    border-width: 1px;
                 }
+
+                .datePart {
+                    float: left;
+                }
+
+                .deleteButtonPart {
+                    float: right;
+                }
+
+                .editButtonPart {
+                    float: right;
+                }
+
 
             `}</style>
         </div>
