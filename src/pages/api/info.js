61f87799d4504ccf70f8234e8a356f1bdc8aa755
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-05-20 18:05:10 -0700

    Add more CORS to info

--- src/pages/api/info.js
+++ src/pages/api/info.js
@@ -7,6 +7,9 @@ handler.use(middleware)
 
 handler.get(async (req, res) => {
 	const info = await req.db.collection("info").findOne({ "key": "info" })
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	res.json(info)
 })
 
