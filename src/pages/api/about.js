b6e632ef6c6f1ed5a76df24cf84698d5d2b53eb0
Hannah Vaughan <vaughanh@oregonstate.edu>
2020-05-20 17:38:59 -0700

    Add headers to about API

--- src/pages/api/about.js
+++ src/pages/api/about.js
@@ -7,6 +7,9 @@ handler.use(middleware)
 
 handler.get(async (req, res) => {
 	let doc = await req.db.collection("about").findOne({ "key": "about" })
+	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	res.json(doc)
 })
 
