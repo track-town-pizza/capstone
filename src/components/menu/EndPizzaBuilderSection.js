0522d3e9aef53104987494eb4440432b58c446ad
hillkai <hillkai@oregonstate.edu>
2021-04-26 19:25:26 -0700

    semantic h tags and link tells sc user that a new window will open

--- src/components/menu/EndPizzaBuilderSection.js
+++ src/components/menu/EndPizzaBuilderSection.js
@@ -7,17 +7,17 @@ const EndPizzaBuilderSection = (props) => {
     return (
         <div>
             <div className="text-center">
-                <h3>Ready to Order?</h3>
+                <h2>Ready to Order?</h2>
                 <div>
                     <p className="mb-0 d-inline">Call <PhoneNumber phoneNumber={props.phoneNumber} linkColor="green"/> or </p>
                     <Link href={props.onlineOrderingLink} className="d-inline">
-                        <a target="_blank" className="text-success">Order Online</a>
+                        <a target="_blank" className="text-success" aria-label="Order online (opens a new window)">Order Online</a>
                     </Link>
                 </div>
             </div>
             <br />
             <div className="text-center">
-                <h3>Want to add another pizza?</h3>
+                <h2>Want to add another pizza?</h2>
                 <YellowFoodButton buttonWord="Build Again" name="Build Again" handleClick={props.handleClick} second=""/>
             </div>
         </div>
