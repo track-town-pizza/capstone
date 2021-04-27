155ef94b23db673c6563cdbf14b8b215d6b68828
hillkai <hillkai@oregonstate.edu>
2021-04-26 19:28:35 -0700

    semantic h tag, better link names, tells sr user that link will open in a new tab

--- src/components/menu/NotOnlineOrdering.js
+++ src/components/menu/NotOnlineOrdering.js
@@ -4,12 +4,12 @@ import PhoneNumber from "../PhoneNumber"
 
 const NotOnlineOrdering = (props) => {
     return (
-        <div className="text-center">
-            <h3>This is not online ordering</h3>
+        <div className="text-center mb-3">
+            <h2 className="font-weight-bold">This is not online ordering</h2>
             <div className="mr-2 ml-2">
-                <p className="d-inline">If you would like to place an order please call <PhoneNumber phoneNumber={props.phoneNumber} linkColor="green"/>  or click </p>
+                <p className="d-inline">If you would like to place an order please call <PhoneNumber phoneNumber={props.phoneNumber} linkColor="green"/>  or go to </p>
                 <Link href={props.onlineOrderingLink}>
-                    <a target="_blank" className="d-inline text-success">here</a>
+                    <a target="_blank" className="d-inline text-success" aria-label="Online Ordering (opens a new window)">Online Ordering</a>
                 </Link>
             </div>
         </div>
