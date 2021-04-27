bd773c6abc59e36502075476f52e3c4d94a5dc96
hillkai <hillkai@oregonstate.edu>
2021-04-26 19:25:45 -0700

    semantic h tag

--- src/components/menu/HalfNHalf.js
+++ src/components/menu/HalfNHalf.js
@@ -5,7 +5,7 @@ import OptionsCheckbox from "./OptionsCheckbox"
 const HalfNHalf = (props) => {
     return (
         <div className="text-center m-4">
-            <h3>Half-and-Half Pizza?</h3>
+            <h2>Half-and-Half Pizza?</h2>
             <OptionsCheckbox name="halfNHalf" onChange={props.onChange} checked={props.halfNHalf} shownWords="Yes" second={props.second}/>
         </div>
     )
